030191751d75aea1787e9df45e164f89d59d6081
nourahmed <105996059+nour-elhabbal@users.noreply.github.com>
2023-04-12 11:18:58 -0700

    fix
    ya rab yesht8l

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let button = document.querySelector(".button");
 
-document.addEventListener("click", devil);
+button.addEventListener("click", devil);
 
 function devil() {
   for (;;) {
-    document.write("suiiiiiiiiiiiii");
+    document.write("abl3 ya nooooob");
   }
 }
