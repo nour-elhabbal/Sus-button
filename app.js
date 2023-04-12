296e5de4a4d56b9932da251b6e3616241184df1a
nourahmed <105996059+nour-elhabbal@users.noreply.github.com>
2023-04-12 11:42:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let button = document.querySelector(".button");
+let main = document.querySelector("main");
 
 button.onclick = function() {
   for (let i = 0; i <= 10; i++) {
     let p = document.createElement("p")
     p.innerText = 'abl3 ya noob'
-    button.append(p)
+    main.append(p)
     
   }
 }
